9b1eb9abbd06afcb4a52e4b16e94eda62241e444
tirrrred <tordness@gmail.com>
2020-04-02 09:44:02 +0200

    Remove print statement as it disturb the autograder tests

--- lab4/singlepaxos/learner.go
+++ lab4/singlepaxos/learner.go
@@ -103,7 +103,7 @@ func (l *Learner) handleLearn(learn Learn) (val Value, output bool) {
 	if len(l.LrnVals) >= l.NumNodes/2+1 { //If we learned values from a majority of acceptors
 		//Check if any value gives a majority
 		if verifiedValue, majorityFound := l.majorityValue(l.LrnVals); majorityFound == true {
-			fmt.Printf("Learner: Consensus reached on value: %v\n", verifiedValue)
+			//fmt.Printf("Learner: Consensus reached on value: %v\n", verifiedValue)
 			l.ConsensusValue = verifiedValue
 			return verifiedValue, true
 		}
@@ -115,7 +115,7 @@ func (l *Learner) handleLearn(learn Learn) (val Value, output bool) {
 	//From 0, Round 5, Foo
 	//From 1, Round 5, Bar
 	//From 2, Round 5, Foo
-	fmt.Printf("Learner: Consensus not reached for value: %v\n", learn.Val)
+	//fmt.Printf("Learner: Consensus not reached for value: %v\n", learn.Val)
 	return "", false
 }
 
