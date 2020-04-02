82452956a0d48543a66c6022c0e6ee1b5d2107c8
tirrrred <tordness@gmail.com>
2020-04-02 13:47:48 +0200

    change logic in increaseCrnd and if prm.Rnd > p.crnd

--- lab4/singlepaxos/proposer.go
+++ lab4/singlepaxos/proposer.go
@@ -187,6 +187,9 @@ func (p *Proposer) constrainedValue(prmReqs []Promise) (val Value, output bool) 
 func (p *Proposer) clientHandler(cVal Value) (prp Prepare, output bool) {
 	leaderNode := p.LeaderDetector.Leader()
 	if leaderNode != p.ID {
+		if prp.Crnd > p.crnd {
+			p.increaseCrnd()
+		}
 		return Prepare{}, false
 	}
 	p.clientValue = cVal
